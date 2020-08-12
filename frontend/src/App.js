03f6aabc1387d4311b613f3c424859b8b66a49f4
Francisco <franciscomsborges2002@gmail.com>
2020-08-12 12:11:49 +0100

    All project

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';//Every .js needs to have React from react
+
+import './global.css';//Import global css config
+
+import Routes from './routes.js';
+
+//The html himself
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
